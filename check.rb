75b207f81ec6910ccf51060aa061a6f081c4deae
Jelle Kralt <jelle@jellekralt.nl>
2014-08-12 15:24:40 +0200

    Added some more info

--- check.rb
+++ check.rb
@@ -6,6 +6,11 @@ require 'ansi_colors'
 domains = File.open('domains.txt').read
 domains.gsub!(/\r\n?/, "\n")
 
+valid = 0
+missing = 0
+
+puts "\n\nChecking domains for 'globalsign.html'\n".ansi_bold
+
 domains.each_line do|n|
 
 	domain = n.gsub(/\s+/, "")
@@ -14,8 +19,14 @@ domains.each_line do|n|
 		check = Net::HTTP.get(domain, '/globalsign.html')
 		if check == "hello globalsign"
 			printf "%-40s [ %s ]\n", domain, "VALID".ansi_green 
+			valid += 1
 		else 
 			printf "%-40s [ %s ]\n", domain, "MISSING".ansi_red 
+			missing += 1
 		end
 	end
 end
+
+puts "\n"
+puts "Found " + valid.to_s.ansi_green.ansi_bold + " domains with a correct globalsign.html file"
+puts "Found " + missing.to_s.ansi_red.ansi_bold + " domains without a correct globalsign.html file"
